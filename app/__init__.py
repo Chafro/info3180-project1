862ae081431c00620bad823d1c85920d1d13666a
Chafro <ChafroBrown@gmail.com>
2020-04-09 04:31:16 -0500

    new database

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 UPLOAD_FOLDER =  "./app/static/uploads"
 
 app = Flask(__name__)
+
 app.config['SECRET_KEY'] = "change this to be a more random key"
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://xmhmzjtroxlkai:6774b90ded624cfc4742e23ab8d501420fbedf369c73b613f3a4c0a127078e40@ec2-54-157-78-113.compute-1.amazonaws.com:5432/d548gt6u5v15ma"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
